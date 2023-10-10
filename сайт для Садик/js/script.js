674b3bda9dc59bbf0dbd33723f4c9dcc1936083a
Saleeh <76786736+saidvelimetov@users.noreply.github.com>
2023-10-10 20:29:38 +0300

    Add files via upload

---
+++ сайт для Садик/js/script.js
@@ -0,0 +1,8 @@
+let index = 0;
+const slides = document.querySelectorAll('.slide');
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 5000);
